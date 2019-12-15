a99e80ee09a4d15e984cd3973e5acf8985747762
Yahya Civelek <mylogicniche@gmail.com>
2019-12-15 07:10:01 +0200

    adding md file in different page

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,27 +1,38 @@
 <template>
-  <q-page class="flex flex-center" style="font-family: Montserrat;">
-    <q-markdown :src="markdown" />
+  <q-page class="inner-page">
+    <q-list bordered padding class="q-mt-xl">
+      <q-item-label header>Blog Yazıları</q-item-label>
+      <q-item v-for="(post,i) in posts" :key="i"
+              clickable v-ripple
+              :to="{name: 'post', params: {name: post.name}}">
+        <q-item-section>
+          <q-item-label>{{post.label}}</q-item-label>
+          <q-item-label caption>{{post.date}}</q-item-label>
+        </q-item-section>
+      </q-item>
+    </q-list>
   </q-page>
 </template>
 
 <style src="@quasar/quasar-ui-qmarkdown/dist/index.css"></style>
 
 <script>
-import { QMarkdown } from '@quasar/quasar-ui-qmarkdown'
-import markdown from '../statics/posts/test.md'
-
 export default {
   data () {
     return {
-      markdown: markdown
+      posts: [
+        { name: 'tavsiyeler.md', label: '10+ Yıllık Gömülü Yazılımcıdan 10+ Tavsiye', date: '15 Aralık 2019' },
+        { name: 'deneme.md', label: 'Deneme', date: '15 Ocak 2020' }
+      ]
     }
-  },
-  components: {
-    QMarkdown
   }
 }
 </script>
 
 <style scoped>
-
+  .inner-page {
+    font-family: Montserrat;
+    max-width: 1000px;
+    margin: auto;
+  }
 </style>
---
+++ src/pages/Post.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page class="flex flex-center" style="font-family: Montserrat;">
+    <q-markdown :is="markdown" :src="markdownn" />
+  </q-page>
+</template>
+
+<style src="@quasar/quasar-ui-qmarkdown/dist/index.css"></style>
+
+<script>
+import { QMarkdown } from '@quasar/quasar-ui-qmarkdown'
+
+export default {
+  data () {
+    return {
+      markdownn: `../statics/posts/${this.$route.params.name}`
+    }
+  },
+  computed: {
+    markdown () {
+      return () => import(`../statics/posts/${this.$route.params.name}`)
+    }
+  },
+  components: {
+    QMarkdown
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
